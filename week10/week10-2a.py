4e7b61b5772586bda799a5c8bb3b739b043c29b3
13751265 <s0958552764@gmail.com>
2026-04-30 15:51:56 +0800

    week10

---
+++ week10/week10-2a.py
@@ -0,0 +1,14 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        def helper(root):
+            if root ==None: return 0 #沒有東西,深度0
+            left=helper(root.left) #左邊的深度
+            right=helper(root.right) #右邊的深度
+            return max(left,right)+1
+        return helper(root)
